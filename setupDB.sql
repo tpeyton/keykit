45782169ffa861e5e412efb4f5ff792b109ae2c5
tpeyton <tpeyton25@gmail.com>
2018-02-28 12:08:37 -0500

    fixed column sizes

--- setupDB.sql
+++ setupDB.sql
@@ -1,5 +1,8 @@
+-- delete database if it already exists
+DROP DATABASE IF EXISTS keykit;
+
 -- create database if it doesn't already exist
-CREATE DATABASE IF NOT EXISTS keykit;
+CREATE DATABASE keykit;
 
 -- grant permissions on database to new keyman user
 GRANT ALL PRIVILEGES ON keykit.* to 'keyman'@'localhost' IDENTIFIED BY 'zooper$secret';
@@ -12,8 +15,8 @@ CREATE TABLE keystore
 ( id INT(11) NOT NULL AUTO_INCREMENT,
   hostname VARCHAR(25),
   ip VARCHAR(15) NOT NULL,
-  ssh_fingerprint VARCHAR(1024),
-  ssh_key VARCHAR(1024),
+  ssh_fingerprint VARCHAR(256),
+  ssh_key VARCHAR(4096),
   creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
   last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   CONSTRAINT keystore_pk PRIMARY KEY (id)

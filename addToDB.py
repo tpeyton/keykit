2e69cd7d5357ab8f74b9beac028242dedb9d687b
tpeyton <tpeyton25@gmail.com>
2018-02-28 11:41:36 -0500

    added host gathering info

--- addToDB.py
+++ addToDB.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
 import MySQLdb
+import socket
+import os
+import subprocess
 
 # initialize sql variables
 sqlUser = "keyman"
@@ -7,10 +10,10 @@ sqlPasswd = "zooper$secret"
 database = "keykit"
 
 # host data
-hostname = "test02"
-ip = "10.0.0.101"
-ssh_key = "234t3gtiwqervceqw234t3grfe234t3grf23r4tgrf32r4gf3"
-fingerprint = "oinrweoqinweroiwer"
+# hostname = "test02"
+# ip = "10.0.0.101"
+# ssh_key = "234t3gtiwqervceqw234t3grfe234t3grf23r4tgrf32r4gf3"
+#fingerprint = "oinrweoqinweroiwer"
 
 # connect to database
 db = MySQLdb.connect("localhost",sqlUser,sqlPasswd,database)
@@ -32,5 +35,28 @@ def addHostToDB(hostname,ip,ssh_key,fingerprint):
 
 	# close connection to db
 	db.close()
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+def get_hostname():
+    return(socket.gethostname())
+
+def calc_sshPubFP():
+    fPrint = subprocess.check_output('ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub', shell=True)
+    fPrint = fPrint.rstrip()
+    return(fPrint)
+
+def get_sshPrivKey():
+    privKey = subprocess.check_output('cat ~/.ssh/id_rsa', shell=True)
+    privKey = privKey.rstrip()
+    return(privKey)
+
+hostname = get_hostname()
+ip = get_ip_address()
+ssh_key = get_sshPrivKey()
+fingerprint = calc_sshPubFP()
 
 addHostToDB(hostname,ip,ssh_key,fingerprint)

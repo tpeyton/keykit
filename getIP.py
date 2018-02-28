dcf758d14b599f3a84eab8c28dd12b3440c9dd4d
tpeyton <tpeyton25@gmail.com>
2018-02-28 11:29:14 -0500

    Chris updated with definitions

--- getIP.py
+++ getIP.py
@@ -2,27 +2,35 @@
 
 import socket
 import os
+import subprocess
+
+def check_if_admin():
+    uid = os.getuid()
+    if(uid != 0):
+    	admin = False
+    if(uid == 0):
+        admin = True
+    return(admin)
 
 def get_ip_address():
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(("8.8.8.8", 80))
-	return s.getsockname()[0]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
 def get_hostname():
-	return(socket.gethostname())
+    return(socket.gethostname())
+
 def get_sshPubKey():
-	key = os.system('cat ~/.ssh/id_rsa.pub')
-	return(key)
+    key = subprocess.check_output('cat ~/.ssh/id_rsa.pub', shell=True)
+    key = key.rstrip()
+    return(key)
+
 def calc_sshPubFP():
-	fPrint = os.system('ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub')
-	return(fPrint)
+    fPrint = subprocess.check_output('ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub', shell=True)
+    fPrint = fPrint.rstrip()
+    return(fPrint)
+
 def get_sshPrivKey():
-	privKey = os.system('cat ~/.ssh/id_rsa')
-	return privKey	
-
-print(get_ip_address())
-print(get_hostname())
-print(get_sshPubKey())
-print(calc_sshPubFP())
-print(get_sshPrivKey())
-
-
+    privKey = subprocess.check_output('cat ~/.ssh/id_rsa', shell=True)
+    privKey = privKey.rstrip()
+    return(privKey)

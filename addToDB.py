0753c713ed854ded6a58a4e8b01ad01e61f53103
tpeyton <tpeyton25@gmail.com>
2018-03-07 17:23:09 -0500

    cleaned up comments, changed to importing dependency files

--- addToDB.py
+++ addToDB.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
+
+# Dependencies
 import MySQLdb
 import socket
 import os
 import subprocess
+
+# file imports
+import getIP
 
 # initialize sql variables
 sqlUser = "keyman"
 sqlPasswd = "zooper$secret"
 database = "keykit"
 
-# host data
-# hostname = "test02"
-# ip = "10.0.0.101"
-# ssh_key = "234t3gtiwqervceqw234t3grfe234t3grf23r4tgrf32r4gf3"
+# test data
+#hostname = "test02"
+#ip = "10.0.0.101"
+#ssh_key = "234t3gtiwqervceqw234t3grfe234t3grf23r4tgrf32r4gf3"
 #fingerprint = "oinrweoqinweroiwer"
 
 # connect to database
@@ -23,8 +28,8 @@ cursor = db.cursor(MySQLdb.cursors.DictCursor)
 
 # This function takes in ssh server characteristics and adds them to our database
 def addHostToDB(hostname,ip,fingerprint,ssh_key):
-	# sql command TODO: update this variable subsitution to best practice for python 3.1? - https://pyformat.info/
-	sql = "INSERT INTO keystore(hostname, ip, ssh_fingerprint, ssh_key) VALUES('%s', '%s', '%s', '%s')" % (hostname, ip, fingerprint, ssh_key)
+	# sql command
+	sql = "INSERT INTO keystore(hostname, ip, ssh_fingerprint, ssh_key) VALUES('{0}', '{1}', '{2}', '{3}')".format(hostname,ip,fingerprint,ssh_key)
 
 	# execute sql command
 	cursor.execute(sql)
@@ -39,7 +44,7 @@ def addHostToDB(hostname,ip,fingerprint,ssh_key):
 
 # search database for queried host and return index if it exists, if not then report error and close
 def searchForHost(hostname):
-	# sql command using best practice variable subsitution for python 3.1
+	# sql command
 	sql = "SELECT * FROM keystore WHERE hostname ='{0}'".format(hostname)
 
 	# execute sql command
@@ -72,35 +77,39 @@ def getHostFromDB(index):
 	# grab the first result
 	result = results[0]
 
-	# output results
+	# output results (will later return variables)
 	print("index: {0} \nname: {1} \nip: {2} \nssh_fingerprint: {3} \nssh_key: {4}".format(result["id"], result["hostname"], result["ip"], result["ssh_fingerprint"], result["ssh_key"]))
 
-def get_ip_address():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
-    return s.getsockname()[0]
+# Test functions for sql query testing
+######################################
+# def get_ip_address():
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     s.connect(("8.8.8.8", 80))
+#     return s.getsockname()[0]
+#
+# def get_hostname():
+#     return(socket.gethostname())
+#
+# def calc_sshPubFP():
+#     fPrint = subprocess.check_output('ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub', shell=True)
+#     fPrint = fPrint.rstrip()
+#     return(fPrint)
+#
+# def get_sshPrivKey():
+#     privKey = subprocess.check_output('cat ~/.ssh/id_rsa', shell=True)
+#     privKey = privKey.rstrip()
+#     return(privKey)
+######################################
 
-def get_hostname():
-    return(socket.gethostname())
+# testing: get details from host
+hostname = getIP.get_hostname()
+ip = getIP.get_ip_address()
+ssh_key = getIP.get_sshPrivKey()
+fingerprint = getIP.calc_sshPubFP()
 
-def calc_sshPubFP():
-    fPrint = subprocess.check_output('ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub', shell=True)
-    fPrint = fPrint.rstrip()
-    return(fPrint)
+# add above test details to last row in DB
+addHostToDB(hostname,ip,fingerprint,ssh_key)
 
-def get_sshPrivKey():
-    privKey = subprocess.check_output('cat ~/.ssh/id_rsa', shell=True)
-    privKey = privKey.rstrip()
-    return(privKey)
-
-# hostname = get_hostname()
-hostname = get_hostname()
-ip = get_ip_address()
-ssh_key = get_sshPrivKey()
-fingerprint = calc_sshPubFP()
-
-#addHostToDB(hostname,ip,fingerprint,ssh_key)
-
+# search for hostname in DB and print details about it
 hostID = searchForHost(hostname)
-print("final id is {}".format(hostID))
 getHostFromDB(hostID)

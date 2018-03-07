23d500799f6036d730e8ed0121b90aaf23b7a3ff
tpeyton <tpeyton25@gmail.com>
2018-03-07 17:29:59 -0500

    removed unused script

--- sshKeyReader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-# Ensure we are running with the proper privileges to access keys (either root or owner of keys)
-
-# if keys exist read them, if not output error message
-
-# TODO: upload keys to database

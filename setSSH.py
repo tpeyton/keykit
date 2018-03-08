1103b28099cfbebeda96151abd1da47c173a4a81
Contonio <36002751+Contonio@users.noreply.github.com>
2018-03-07 20:30:39 -0500

    Added working setSSH.py

--- setSSH.py
+++ setSSH.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python
 
-import socket
-import os
 import subprocess
 import argparse
 import getIP
+import os
 from pathlib import Path
 
 adminStatus = getIP.check_if_admin()
-backupFile = Path("~/.ssh/id_rsa.bak")
 
 def set_sshPrivKey(sshKey):
-        if(adminStatus == True):
-                subprocess.run("mv ~/.ssh/id_rsa ~/.ssh/id_rsa.bak", shell=True)
-                if(backupFile.is_file()):
-                        subprocess.run("echo", sshKey, ">", "~/.ssh/id_rsa.bak")
-                        if(getIP.get_sshPrivKey() == sshKey):
-                                print("Success.")
-                else:
-                        print("Error creating backup. Please try again.")
-        else:
-                print("You do not have the proper permissions to perform this command. Please re-run t$
+	if(adminStatus == True):
+		subprocess.call("mv ~/.ssh/id_rsa ~/.ssh/id_rsa.bak", shell=True)
+		print("Backup stored successfully as ~/.ssh/id_rsa.bak")
+		subprocess.call("echo {} > ~/.ssh/id_rsa".format(sshKey), shell=True)
+		if(getIP.get_sshPrivKey() == sshKey):
+				print("Success! The SSH private key was changed.")
+		else:
+				print("Error: The SSH private key was not replaced successfully.")
+	else:
+		print("You do not have the proper permissions to perform this command. Please re-run this command as sudo.")
 
-set_sshPrivKey("test")
+
+
+

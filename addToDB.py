891c2c3ead39ebf9b21e6cab188196e65a90bbe1
tpeyton <tpeyton25@gmail.com>
2018-03-08 01:18:53 -0500

    reorganized comments

--- addToDB.py
+++ addToDB.py
@@ -26,6 +26,7 @@ db = MySQLdb.connect("localhost",sqlUser,sqlPasswd,database)
 # initialize the cursor and use dict mode so we can search by column name
 cursor = db.cursor(MySQLdb.cursors.DictCursor)
 
+# TODO: Consolidate the following sql related functions to avoid repeating all the duplicate code
 # This function takes in ssh server characteristics and adds them to our database
 def addHostToDB(hostname,ip,fingerprint,ssh_key):
 	# sql command
@@ -34,6 +35,7 @@ def addHostToDB(hostname,ip,fingerprint,ssh_key):
 	# execute sql command
 	cursor.execute(sql)
 
+	# DEBUG: make sure data was successfully added to db
 	print(cursor.fetchall())
 
 	# save changes to db
@@ -50,10 +52,10 @@ def searchForHost(hostname):
 	# store results in variables
 	results = cursor.fetchall()
 
-	# we only care about the first result so let's filter to that
-	result = results[0]
-
 	# TODO: handle empty results
+
+	# we only care about the first result so grab that
+	result = results[0]
 
 	# DEBUG: Output results
 	#print("index: {0} name: {1}".format(result["id"], result["hostname"]))
@@ -69,6 +71,7 @@ def getHostFromDB(index):
 	# execute sql command
 	cursor.execute(sql)
 
+	# Retrieves results
 	results = cursor.fetchall()
 
 	# grab the first result
@@ -77,7 +80,7 @@ def getHostFromDB(index):
 	# output results (will later return variables)
 	print("index: {0} \nname: {1} \nip: {2} \nssh_fingerprint: {3} \nssh_key: {4}".format(result["id"], result["hostname"], result["ip"], result["ssh_fingerprint"], result["ssh_key"]))
 
-# testing: get details from host
+# testing: get details from localhost
 hostname = getIP.get_hostname()
 ip = getIP.get_ip_address()
 ssh_key = getIP.get_sshPrivKey()

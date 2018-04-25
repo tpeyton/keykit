2a0abb67dcdffc199420ceaea0d93ca9bdd95de7
tpeyton <tpeyton25@gmail.com>
2018-04-25 11:16:11 -0400

    broken

--- keykit.py
+++ keykit.py
@@ -55,11 +55,10 @@ if args.find:
         # Ensure a result was found before attempting to search the db
         if(hostID != 0):
             # get the result
-        	result = dbFunctions.getHostFromDB(db,cursor,hostID)
             print("{}".format(result["ssh_key"]))
             print(hostID)
         else:
-        	print("No results found.")
+            print("no results found.")
 
 # search by ip
 if args.findIP:
